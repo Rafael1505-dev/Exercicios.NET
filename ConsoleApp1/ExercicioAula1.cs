30cc76803e123a2b75c42bef25e7fee9fb139839
rafin <rafin@LAPTOP-L8DCFM95>
2024-06-26 20:53:27 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/ExercicioAula1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exercicios.NET
+{
+    class Exercicio1
+    {
+        public void ExercicioAula1()
+        {
+            // Exercício de entrada de dados em C#
+
+            Console.WriteLine("\n===== Exercício de entrada de dados em C# =======\n\n");
+
+
+            Console.WriteLine("Entre com seu nome completo:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Quantos quartos você tem na sua casa?");
+            int badroom = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço dos produtos:");
+           double price = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+            string[] vet = Console.ReadLine().Split(' ');
+            string lastName = vet[0];
+            int age = int.Parse(vet[1]);
+            double height = double.Parse(vet[2]);
+
+            Console.WriteLine("\n===== RESULTADO DO EXERCÍCIO =======\n\n");
+
+            Console.WriteLine(name);
+            Console.WriteLine(badroom);
+            Console.WriteLine(price);
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+
+            // Exercício de entrada de dados em C#
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("===== Menu =====");
+                Console.WriteLine("1. Executar Exercicio 1");
+                Console.WriteLine("0. Sair");
+                Console.Write("Escolha uma opção: ");
+
+                string escolha = Console.ReadLine();
+
+                switch (escolha)
+                {
+                    case "1":
+                        Exercicio1 exercicio1 = new Exercicio1();
+                        exercicio1.ExercicioAula1();
+                        break;
+                    case "0":
+                        return; // Sai do programa
+                    default:
+                        Console.WriteLine("Opção inválida! Tente novamente.");
+                        break;
+                }
+
+                Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
+                Console.ReadKey();
+            }
+        }
+    }    
+}
