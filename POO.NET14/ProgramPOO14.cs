dceb8b6f6900afa28638389a30576135ed23ffab
Rafael1505-dev <76927352+Rafael1505-dev@users.noreply.github.com>
2024-10-01 23:55:40 -0300

    Ex 14

--- POO.NET14/ProgramPOO14.cs
+++ POO.NET14/ProgramPOO14.cs
@@ -1,4 +1,5 @@
 ﻿using POO.NET14.Entities;
+using POO.NET14.Services;
 using System.Collections.Generic;
 using System.Globalization;
 
@@ -8,7 +9,28 @@ namespace POO.NET14
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("\n===== Exercício de Produtos com Herança e Polimorfismo em C# =======\n\n");
+            Console.WriteLine("\n===== Exercício com interface em C# =======\n\n");
+
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract myContract = new Contract(contractNumber, contractDate, contractValue);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(myContract, months);
+
+            Console.WriteLine("Installments:");
+            foreach (Installment installment in myContract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
         }
     }
 }
